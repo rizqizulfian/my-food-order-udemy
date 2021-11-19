90a07e05cc1b0b7e9384a413b8dd5d370f138853
venln4 <rizqi.ZULFIAN@uobgroup.com>
2021-11-19 21:30:09 +0700

    [Feature-136] - Adding MealItem

---
+++ src/modules/Meals/MealItem/MealItem.js
@@ -0,0 +1,20 @@
+import classes from './MealItem.module.css';
+
+const MealItem = (props) => {
+  const priceMeal = `Rp. ${props.price.toFixed(2)}`;
+
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3>{props.name}</h3>
+        <div className={classes.description}>{props.description}</div>
+        <div className={classes.price}>{priceMeal}</div>
+      </div>
+      <div>
+
+      </div>
+    </li>
+  )
+};
+
+export default MealItem;
