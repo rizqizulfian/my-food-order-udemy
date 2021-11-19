e5ab38d10367c1c6a3c74b18d1dd606bd51cb5c2
venln4 <rizqi.ZULFIAN@uobgroup.com>
2021-11-19 23:33:14 +0700

    [Feature-144] - Adding a Cart Reducer, yang sebelumnya harusnya using context judulnya

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,12 +1,38 @@
+import { useReducer } from 'react';
 import CartContext from './cart-context';
 
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  // disini si action ada 2 obj, type = pembeda jenisnya, item adalah itemnya
+  if (action.type === 'ADD_CART_ITEM') {
+    // disini kita pakai concat agar mengembalikan array baru dan tidak mengubah array lama si statenya
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+  return defaultCartState;
+};
+
 const CartProvider = props => {
-  const addItemToCartHandler = item => { };
-  const removeItemFromCartHandler = id => { };
+  const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+
+  const addItemToCartHandler = item => {
+    dispatchCartAction({type: 'ADD_CART_ITEM', item: item});
+  };
+  const removeItemFromCartHandler = id => {
+    dispatchCartAction({type: 'REMOVE_CART_ITEM', item: item});
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.item,
+    totalAmount: cartState.amount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   }
