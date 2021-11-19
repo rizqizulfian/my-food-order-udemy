16108d21d0c5c2e29619f8ecf6b71a34234bfe99
venln4 <rizqi.ZULFIAN@uobgroup.com>
2021-11-20 01:37:48 +0700

    [Feature-147] - Working on a more complex reducer logic

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -9,14 +9,32 @@ const defaultCartState = {
 const cartReducer = (state, action) => {
   // disini si action ada 2 obj, type = pembeda jenisnya, item adalah itemnya
   if (action.type === 'ADD_CART_ITEM') {
-    // disini kita pakai concat agar mengembalikan array baru dan tidak mengubah array lama si statenya
-    const updatedItems = state.items.concat(action.item);
     const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+
+    // code to process if the same item is already in cart
+    const existingCartItemIndex = state.items.findIndex(
+      item => item.id === action.item.id
+    );
+    const existingCartItem = state.items[existingCartItemIndex];
+    
+    let updatedItems;
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      }
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
+
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
     };
   }
+
   return defaultCartState;
 };
 
@@ -24,10 +42,10 @@ const CartProvider = (props) => {
   const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
 
   const addItemToCartHandler = (item) => {
-    dispatchCartAction({type: 'ADD_CART_ITEM', item: item});
+    dispatchCartAction({ type: 'ADD_CART_ITEM', item: item });
   };
   const removeItemFromCartHandler = (id) => {
-    dispatchCartAction({type: 'REMOVE_CART_ITEM', item: id});
+    dispatchCartAction({ type: 'REMOVE_CART_ITEM', item: id });
   };
 
   const cartContext = {
