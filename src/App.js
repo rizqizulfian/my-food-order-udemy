b8949a86610d1d1cfcbc539bcfb65386008e4159
venln4 <rizqi.ZULFIAN@uobgroup.com>
2021-11-19 22:17:42 +0700

    [Feature-150] - Adding Backdrop

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { Fragment } from 'react';
 
 import Header from './modules/Layout/Header';
 import Meals from './modules/Meals/Meals';
+import Cart from './modules/Cart/Cart';
 
 function App() {
   return (
     <Fragment>
+      <Cart />
       <Header />
       <main>
         <Meals />
