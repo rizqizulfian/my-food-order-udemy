e46aea8057cfedb448f936cda6c715319c8ec494
venln4 <rizqi.ZULFIAN@uobgroup.com>
2021-11-20 02:07:24 +0700

    [Feature-148] - Making item removable

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -35,6 +35,32 @@ const cartReducer = (state, action) => {
     };
   }
 
+  if (action.type === 'REMOVE_CART_ITEM') {
+    const existingCartItemIndex = state.items.findIndex(
+      item => item.id === action.id
+    );
+    console.log('ini state itemsnya', state.items, action.id)
+    console.log('ini state index', existingCartItemIndex)
+    const existingItem = state.items[existingCartItemIndex];
+    console.log('ini exi', existingItem)
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+
+    let updatedItems;
+    if (existingItem.amount === 1) {
+      console.log('ini masuk if', updatedItems)
+      updatedItems = state.items.filter(item => item.id !== action.id)
+      console.log('ini masuk if 2', updatedItems, state.items.filter(item => item.id !== action.id))
+    } else {
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      updatedItems = [ ...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    }
+  }
   return defaultCartState;
 };
 
@@ -45,7 +71,7 @@ const CartProvider = (props) => {
     dispatchCartAction({ type: 'ADD_CART_ITEM', item: item });
   };
   const removeItemFromCartHandler = (id) => {
-    dispatchCartAction({ type: 'REMOVE_CART_ITEM', item: id });
+    dispatchCartAction({ type: 'REMOVE_CART_ITEM', id: id });
   };
 
   const cartContext = {
